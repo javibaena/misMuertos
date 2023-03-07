4f399f355ae1bc6523e08fd9cb85d908f8efbba3
javibaena <javibaenatro15@gmail.com>
2023-03-07 22:02:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Testimonio from './componentes/Testimonio.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Testimonio></Testimonio>
+    </div>
+  );
+}
+
+export default App;
