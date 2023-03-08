9d23ce6a805218331e0f3b3f06c5048e55e4679a
javibaena <javibaenatro15@gmail.com>
2023-03-08 18:49:17 +0100

    nuevas actualizaciones

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -14,8 +14,11 @@ function Testimonio (){
         <div className='contenedor-texto-textimonio'>
             <p className='nombre-testimonio'>Emma tokisha</p>
             <p className='cargo-testimonio'>bombero</p>
+            <p className='cargo-testimonio'>bombero</p>
+            <p className='cargo-testimonio'>bombero</p>
+            <p className='cargo-testimonio'>actualizacion</p>
             <p className='texto-testimonio'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in</p>
-        
+
         </div>
       </div>
   )
